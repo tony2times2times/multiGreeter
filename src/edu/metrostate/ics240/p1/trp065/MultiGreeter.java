88f3e59872bdd415cd8608fb396d510ba2587f21
tony2times2times <giperaza@yahoo.com>
2017-05-18 11:13:04 -0500

    minor gramar changes

--- src/edu/metrostate/ics240/p1/trp065/MultiGreeter.java
+++ src/edu/metrostate/ics240/p1/trp065/MultiGreeter.java
@@ -4,7 +4,7 @@
 //Assignment 1 
 //https://github.com/tony2times2times/multiGreeter
 
-package MultiGreeter;
+package edu.metrostate.ics240.p1.trp065;
 
 import java.util.Random;
 
@@ -27,7 +27,7 @@ public class MultiGreeter {
 		if (size > 0) {
 			greetings = new String[size];
 		}
-		// If someone entered a number less than zero, use the default size.
+		// If someone entered a negative number use the default size.
 		else {
 			greetings = new String[DEFUALT_SIZE];
 		}
@@ -43,18 +43,17 @@ public class MultiGreeter {
 
 	// Method to get total number of greetings in the greetings array, excluding null values.
 	public int getNumGreetings() {
-		// if no greetings have been added there should only be the default
-		// return 1
+		// If no greetings have been added there should only be the default greeting; return 1.
 		if (defaultGreet == true) {
 			return 1;
 		}
-		// if greetings have been added return the greet count.
+		// If greetings have been added return the greet count.
 		else {
 			return (greetCount);
 		}
 	}
 
-	// method takes a string as input and adds it to the greetings array. I wish java had a array.push() method.
+	// Method takes a string as input and adds it to the greetings array. I wish java had a array.push() method.
 	public boolean addGreeting(String greeting) {
 		// If there is space left in the array then add the greeting.
 		if (greetings.length >= greetCount) {
@@ -79,9 +78,10 @@ public class MultiGreeter {
 	public String greet(String name) {
 		Random rand = new Random();
 		if (defaultGreet) {
-			return (greetings[0] + " " + name);
+			return (greetings[0] + ", " + name + "!");
 		} else {
-			return (greetings[rand.nextInt(greetCount)] + " " + name);
+			int index = rand.nextInt(greetCount);
+			return (greetings[index] + ", " + name + "!");
 		}
 	}
 
@@ -89,9 +89,10 @@ public class MultiGreeter {
 	public String greet() {
 		Random rand = new Random();
 		if (defaultGreet) {
-			return (greetings[0] + " World");
+			return (greetings[0] + ", World!");
 		} else {
-			return (greetings[rand.nextInt(greetCount)] + " World");
+			int index = rand.nextInt(greetCount);
+			return (greetings[index] + ", World!");
 		}
 	}
 }
--- src/edu/metrostate/ics240/p1/trp065/MultiGreeterTest.java
+++ src/edu/metrostate/ics240/p1/trp065/MultiGreeterTest.java
@@ -4,10 +4,9 @@
 //Test for Assignment 1 
 //https://github.com/tony2times2times/multiGreeter
 
-package MultiGreeter.tests;
+package edu.metrostate.ics240.p1.trp065;
 
 import java.util.Arrays;
-import MultiGreeter.MultiGreeter;
 
 public class MultiGreeterTest {
 
@@ -26,7 +25,7 @@ public class MultiGreeterTest {
 		//Test adding greetings to the array.
 		hello.addGreeting("konichiwa");
 		//test the greet Methods both with and without arguments 
-		if (!hello.greet().equals("konichiwa World") || !hello.greet("me").equals("konichiwa me")) {
+		if (!hello.greet().equals("konichiwa, World!") || !hello.greet("me").equals("konichiwa, me!")) {
 			fubar = true;
 			System.out.println("Unexpected result found in hello.addGreeting('konichiwa')");
 		}
@@ -36,20 +35,19 @@ public class MultiGreeterTest {
 		//Set tester to a no arguments greeting.
 		String tester = hello.greet();
 		//Make sure tester is one of the two added greetings and that there are only two greetings.
-		if (!(tester.equals("konichiwa World") || tester.equals("ohayo gozaimasu World"))
+		if (!(tester.equals("konichiwa, World!") || tester.equals("ohayo gozaimasu, World!"))
 				|| hello.getNumGreetings() != 2) {
 			fubar = true;
 			System.out.println("Unexpected result found in hello.addGreeting('ohayo gozaimasu')");
 		}
 		
 		//Test creating a MultiGreeter with an array size of 2
-		MultiGreeter goodbye = new MultiGreeter(0);
+		MultiGreeter goodbye = new MultiGreeter(2);
 		goodbye.addGreeting("sayonara");
 		goodbye.addGreeting("jaa ne");
 		//Convert the array to a string so the results can be tested.
 		String goodbyeString = Arrays.toString(goodbye.getGreetings());
-		System.out.println(goodbyeString);
-		//Compare goodbyeString to the expected output.
+		//Compare goodbyeString to the expected output. This tests the getGreetings method.
 		if (!goodbyeString.equals("[sayonara, jaa ne]")) {
 			fubar = true;
 			System.out.println("Unexpected result found in goodbye.getGreetings()");
